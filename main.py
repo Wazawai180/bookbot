c5f2c3cf134890c614b1528e7a48939d51d4712b
Wazawai180 <wazawai180@gmail.com>
2025-03-04 10:22:13 -0800

    add new character_count function to stats, updated main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import character_count
 
 def get_book_text(path_to_file):
     try:
@@ -14,9 +15,11 @@ def main():
     filepath = "/home/koreypatterson/workspace/github.com/bookbot/books/frankenstein.txt"
     book_text = get_book_text(filepath)
     word_count = get_word_count(book_text)
+    all_char = character_count(book_text)
 
     if book_text is not None:
         print(f"{word_count} words found in the document")
+        print(all_char)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_word_count(file_contents):
     word_count = len(file_contents.split())
     return word_count
+
+def character_count(file_contents):
+    file_contents = file_contents.lower()
+    char_count = {}
+    for char in file_contents: #Iterate over text
+        if char in char_count: #If the char is already in dict increment
+            char_count[char] += 1
+        else: #If not, add char with count 1
+            char_count[char] = 1
+    return char_count
